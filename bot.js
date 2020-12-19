fb324b939a380d45ec0f6cbe5d86209c773606d3
Johnny <72969582+JohnnyD2020@users.noreply.github.com>
2020-12-19 16:13:51 +1100

    bot.js
    the code for a discord bot!

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+bot_secret_token = "paste token here!"
+
+client.login(bot_secret_token)
+
+// for the bot to work type in your terminal or command prompt node bot.js 
